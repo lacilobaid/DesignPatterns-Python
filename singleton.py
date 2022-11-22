c9302a1db1e847a2eb74f9b01db78028ac84472b
Seyma <63148309+lacilobaid@users.noreply.github.com>
2022-11-22 13:00:54 +0300

    Create singleton.py

--- singleton.py
+++ singleton.py
@@ -4,10 +4,11 @@ import firebase_admin
 from firebase_admin import credentials,firestore
 
 class Singleton(type):
+
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
+        if not cls._instances:
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
         return cls._instances[cls]
@@ -22,6 +23,7 @@ class member(metaclass=Singleton):
             print("Baglantı basarısız...")
 
     def login(self):
+        print("** Kayit **")
         doc_len = list(self.db.collection('uyeler').get())
         i = len(doc_len)
         doc_name = 'hesap' + str(i+1)
@@ -39,6 +41,7 @@ class member(metaclass=Singleton):
                 try:
                     self.db.collection('uyeler').document(doc_name).set(data)
                     print("Kayıt işlemi basarili. Giriş yapabilirsiniz...")
+                    self.signin()
                 except:
                     print("Kayıt basarısız...")
             else:
@@ -46,35 +49,44 @@ class member(metaclass=Singleton):
         else:
             print("Hatali bir telefon numarası girdiniz.")
 
-    def signin(self,kullanici_adi,sifre):
-        doc_kullaniciAdi = self.db.collection('uyeler').where("kullanici_Adi", "==", kullanici_adi).get()
-        doc_sifre = self.db.collection('uyeler').where("Sifre", "==", sifre).get()
-        if doc_kullaniciAdi:
-            if doc_sifre:
-                print("Giriş başarılı",kullanici_adi)
-                return True
+    def signin(self):
+        print("** Giris **")
+        while True:
+            kullanici_adi = input("Kullanıcı adı giriniz: ")
+            sifre = input("Sifre giriniz: ")
+            doc_kullaniciAdi = self.db.collection('uyeler').where("kullanici_Adi", "==", kullanici_adi).get()
+            if doc_kullaniciAdi:
+                doc_sifre = self.db.collection('uyeler').where("Sifre", "==", sifre).get()
+                if doc_sifre:
+                    print("Giriş başarılı", kullanici_adi)
+                    break
+                else:
+                    print("Sifre yanlıs.")
             else:
-                print("Sifre yanlıs")
-        else:
-            print("Bu isimde bir kullanıcı yoktur")
+                print("Bu isimde bir kullanıcı yoktur.")
+                return False
+
 
 if __name__ == '__main__':
+
    while True:
        m1 = member()
        m2 = member()
        print("**************************************")
-       giris = input("1. Web Kayıt \n2. Web Giris\n3. Mobil Kayıt \n4. Mobil Giris \nQ. Cıkıs ")
+       giris = input("1. Web Kayıt/Giris\n2. Mobil Kayıt/Giris \nQ. Cıkıs ")
        if giris == "q" or giris == "Q":
+           print("Cikis yapiliyor...")
+           time.sleep(2)
            break
        elif giris == "1":
-           m1.login()
+           kontrol=m1.signin()
+           if kontrol==False:
+               time.sleep(2)
+               m1.login()
        elif giris=="2":
-           kullanici_adi = input("Kullanıcı adı giriniz: ")
-           sifre = input("Sifre giriniz: ")
-           m2.signin(kullanici_adi, sifre)
-       elif giris=="3":
-           m1.login()
-       elif giris=="4":
-           kullanici_adi = input("Kullanıcı adı giriniz: ")
-           sifre = input("Sifre giriniz: ")
-           m2.signin(kullanici_adi, sifre)
+           kontrol = m2.signin()
+           if kontrol==False:
+               time.sleep(2)
+               m2.login()
+
+
