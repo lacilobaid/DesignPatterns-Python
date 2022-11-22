0f3483ef3adfa32e9aef8c45d57e7f1e3b8d6ded
Seyma <63148309+lacilobaid@users.noreply.github.com>
2022-11-22 16:27:06 +0300

    Update observer.py

--- observer.py
+++ observer.py
@@ -2,8 +2,7 @@ import sys
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
+from win10toast import ToastNotifier
 
 class selenium_baglan:
 
@@ -22,50 +21,25 @@ class selenium_baglan:
 
 
 class veri_Foreks:
-
     kripto_fiyat= set()
-
     def ekle(self,fiyat):
         self.kripto_fiyat.add(fiyat)
-
     def bilgi(self,mesaj,ad):
         for i in self.kripto_fiyat:
             i.bilgi_yazdir(mesaj,ad)
 
 class observer:
-
-    def bilgi_yazdir(self,mesaj,ad): #  29 kasım 2022deki değerlere göre kosullar belirtildi
-        if mesaj < "20.767":
-            print(ad+" kriptosu düşmüştür: "+mesaj)
-        elif mesaj > "20.767":
-            print(ad+" kriptosu yükselmistir: "+mesaj)
-        elif mesaj < "1.623":
-            print(ad + " kriptosu düşmüştür: " + mesaj)
-        elif mesaj > "1.623":
-            print(ad + " kriptosu yükselmistir: " + mesaj)
-        elif mesaj < "301,7":
-            print(ad+" kriptosu düşmüştür: "+mesaj)
-        elif mesaj > "301,7":
-            print(ad+" kriptosu yükselmistir: "+mesaj)
-        elif mesaj < "1":
-            print(ad+" kriptosu düşmüştür: "+mesaj)
-        elif mesaj > "1":
-            print(ad+" kriptosu yükselmistir: "+mesaj)
-        elif mesaj < "33,13":
-            print(ad+" kriptosu düşmüştür: "+mesaj)
-        elif mesaj > "33,13":
-            print(ad+" kriptosu yükselmistir: "+mesaj)
+    bildirim = ToastNotifier()
+    def bilgi_yazdir(self,mesaj,ad):
+        self.bildirim.show_toast(title=ad, msg= mesaj, duration=5)
 
 if __name__ == '__main__':
     baglan = selenium_baglan()
-    print("**************************")
-    time.sleep(2)
     foreks = veri_Foreks()
     os = observer()
     foreks.ekle(os)
-    for i in range(0,len(baglan.kurlar)):
-        foreks.bilgi(baglan.kurlar[i],baglan.kurlar2[i])
-    time.sleep(5)
+    for i in range(0, len(baglan.kurlar)):
+        foreks.bilgi(baglan.kurlar[i], baglan.kurlar2[i])
     baglan.browser.close()
 
 #*********************************************************
@@ -115,3 +89,5 @@ if __name__ == '__main__':
     print('Tweet gonderme basarili...')
     time.sleep(2)
     twitter.bilgilendir(atilanTwit)
+
+
