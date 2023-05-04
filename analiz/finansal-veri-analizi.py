583a9180f34feff2337abb517a5d0d45d287de37
Seyma <63148309+seymagkts@users.noreply.github.com>
2023-05-04 17:53:19 +0300

    Update finansal-veri-analizi.py

--- analiz/finansal-veri-analizi.py
+++ analiz/finansal-veri-analizi.py
@@ -3,9 +3,37 @@ import pandas as pd
 from scipy import stats
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_excel("finansal_dataset.xlsx")
 
+class analiz:
+    
+    def deger_oku(self,dizi,metin,dataset):
+        eleman = dataset[metin].values
+        for i in eleman:
+            dizi.append(i)
+        
+    def getiri_hesapla(self,dizi,deger): # pct_change()
+        for y in deger:
+            if deger.index(y) == 0:
+                getiri = 0
+            else:
+                getiri = np.log(deger[deger.index(y)] / deger[(deger.index(y))-1])
+            dizi.append(getiri)
+    
+    def faiz_aylik(self,deger,dizi):
+        for z in dizi:
+            eleman = z/12
+            deger.append(eleman)
+            
+    def asiri_hesapla(self,getiri,faiz,dizi):
+        for j in range(0,154):
+            asiri = (getiri[j]-faiz[j])*100
+            dizi.append(asiri)
+    
+analiz = analiz()
 TUPRS_deger = []
 BIST100_deger = []
 kur_deger = []
@@ -17,21 +45,16 @@ arzm3_deger = []
 sue_deger = []
 faiz_deger = []
 
-def deger_oku(dizi,metin,dataset):
-    eleman = dataset[metin].values
-    for i in eleman:
-        dizi.append(i)
-        
-deger_oku(TUPRS_deger,"TUPRS",dataset)
-deger_oku(BIST100_deger,"BIST100",dataset)
-deger_oku(kur_deger,"USD/TRY",dataset)
-deger_oku(petrol_deger,"PETROL",dataset)
-deger_oku(altin_deger,"ALTIN",dataset)
-deger_oku(tufe_deger,"TUFE",dataset)
-deger_oku(arzm2_deger,"M2",dataset)
-deger_oku(arzm3_deger,"M3",dataset)
-deger_oku(sue_deger,"SUE",dataset)
-deger_oku(faiz_deger,"FAIZ",dataset)
+analiz.deger_oku(TUPRS_deger,"TUPRS",dataset)
+analiz.deger_oku(BIST100_deger,"BIST100",dataset)
+analiz.deger_oku(kur_deger,"USD/TRY",dataset)
+analiz.deger_oku(petrol_deger,"PETROL",dataset)
+analiz.deger_oku(altin_deger,"ALTIN",dataset)
+analiz.deger_oku(tufe_deger,"TUFE",dataset)
+analiz.deger_oku(arzm2_deger,"M2",dataset)
+analiz.deger_oku(arzm3_deger,"M3",dataset)
+analiz.deger_oku(sue_deger,"SUE",dataset)
+analiz.deger_oku(faiz_deger,"FAIZ",dataset)
     
 
 TUPRS_getiri = []
@@ -46,32 +69,19 @@ sue_getiri = []
 faiz = []
 faiz_getiri = []
 
-def faiz_aylik(deger,dizi):
-    for z in dizi:
-        eleman = z/12
-        deger.append(eleman)
-    
-faiz_aylik(faiz,faiz_deger)
+analiz.faiz_aylik(faiz,faiz_deger)
 
 # getiri hesaplarının yapılması
-def getiri_hesapla(dizi,deger): # pct_change()
-    for y in deger:
-        if deger.index(y) == 0:
-            getiri = 0
-        else:
-            getiri = np.log(deger[deger.index(y)] / deger[(deger.index(y))-1])
-        dizi.append(getiri)
-
-getiri_hesapla(TUPRS_getiri,TUPRS_deger)
-getiri_hesapla(BIST100_getiri,BIST100_deger)
-getiri_hesapla(kur_getiri,kur_deger)
-getiri_hesapla(petrol_getiri,petrol_deger)
-getiri_hesapla(altin_getiri,altin_deger)
-getiri_hesapla(tufe_getiri,tufe_deger)
-getiri_hesapla(arzm2_getiri,arzm2_deger)
-getiri_hesapla(arzm3_getiri,arzm3_deger)
-getiri_hesapla(sue_getiri,sue_deger)
-getiri_hesapla(faiz_getiri,faiz)
+analiz.getiri_hesapla(TUPRS_getiri,TUPRS_deger)
+analiz.getiri_hesapla(BIST100_getiri,BIST100_deger)
+analiz.getiri_hesapla(kur_getiri,kur_deger)
+analiz.getiri_hesapla(petrol_getiri,petrol_deger)
+analiz.getiri_hesapla(altin_getiri,altin_deger)
+analiz.getiri_hesapla(tufe_getiri,tufe_deger)
+analiz.getiri_hesapla(arzm2_getiri,arzm2_deger)
+analiz.getiri_hesapla(arzm3_getiri,arzm3_deger)
+analiz.getiri_hesapla(sue_getiri,sue_deger)
+analiz.getiri_hesapla(faiz_getiri,faiz)
 
 # getirilerin tabloya sutun olarak kaydetme
 dataset["rTUPRS"]=np.array(TUPRS_getiri)
@@ -90,22 +100,27 @@ dataset["rAyfaiz"]=np.array(faiz_getiri)
 BIST100_asiri = []
 TUPRS_asiri = []
 
-def asiri_hesapla(getiri,faiz,dizi):
-    for j in range(0,154):
-        asiri = (getiri[j]-faiz[j])*100
-        dizi.append(asiri)
-    
-asiri_hesapla(BIST100_getiri,faiz_getiri,BIST100_asiri)
-asiri_hesapla(TUPRS_getiri,faiz_getiri,TUPRS_asiri)   
+analiz.asiri_hesapla(BIST100_getiri,faiz_getiri,BIST100_asiri)
+analiz.asiri_hesapla(TUPRS_getiri,faiz_getiri,TUPRS_asiri)   
 
 # aşırıları tabloya sutun olarak kaydetme
 dataset["erBIST100"]=np.array(BIST100_asiri)
 dataset["erTUPRS"]=np.array(TUPRS_asiri)
 
 # regresyon
+# stats ile model kurma
 bagimli_degisken=dataset["erTUPRS"]
 etkenler = dataset[["erBIST100","rKur","rPetrol","rAltin","Enflasyon","rM2","rM3","rSue","rAyfaiz"]]
 
 sabit = sm.add_constant(etkenler)
 model_arb = sm.OLS(bagimli_degisken,sabit).fit()
 model_arb.summary()
+
+
+# sklearn ile model kurma
+lm = LinearRegression()
+bagimli = dataset[["erTUPRS"]]
+bagimsiz = dataset[["erBIST100","rKur","rPetrol","rSue","rM2","rM3","Enflasyon","rAltin","rAyfaiz"]]
+
+model = lm.fit(bagimsiz,bagimli)
+
